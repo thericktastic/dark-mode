68d2b13063e1c613ee97d7e454147fe1f490f67e
thericktastic <love@The-RT-MBP.attlocal.net>
2020-01-16 10:29:58 -0500

    Adding comments to reinforce understanding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useDarkMode } from "../hooks/useDarkMode";
 // This code renders the NavBar element and contains the switch for dark mode functionality.
 const Navbar = () => {
   // Rather than using setState,
-  const [darkMode, setDarkMode] = useDarkMode(false);
+  const [darkMode, setDarkMode] = useDarkMode('key');
   console.log(darkMode);
 
   const toggleMode = e => {
@@ -12,6 +12,7 @@ const Navbar = () => {
     setDarkMode(!darkMode);
   };
 
+  // This code returns the Navbar component 
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -6,13 +6,19 @@ export const useDarkMode = (key, initialValue) => {
   // This calls useLocalStorage and passes in the key I want to use to store whether or not dark mode is enabled.
   // This hook returns an array with a value and a setter in an array, exactly like the state hook.
   const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+  console.log("This is darkMode: ", darkMode)
+  console.log("This is setDarkMode: ", setDarkMode)
 
+  //  useEffect is implemented here to add/remove the dark-mode class to the body element.
+  // The if statement checks to see if (darkMode === true), which represents the switch being turned on (false = turned off).
+  // If the switch is on/true, the if statement attaches the dark-mode class to the body element. If the switch is off/false, the if statement removes the dark-mode class.
   useEffect(() => {
     if (darkMode) {
       document.querySelector("body").classList.add("dark-mode");
     } else {
       document.querySelector("body").classList.remove("dark-mode");
     }
+    // the dependency array is set to darkMode so useEffect is watching the value of darkMode to render the DOM
   }, [darkMode]);
 
   return [darkMode, setDarkMode];
